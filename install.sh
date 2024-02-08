84590b3b39428f1c174cbd155314d4e6aee800a4
smaghili <97967312+smaghili@users.noreply.github.com>
2024-02-09 01:40:00 +0330

    add install.sh and wgt.sh with fix unicode

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set the GitHub repository URL
+repo_url="https://github.com/smaghili/WireGuard-Tunnel.git"
+
+# Clone the GitHub repository
+git clone "$repo_url"
+
+# Navigate to the cloned repository directory
+
+
+# Set execute permissions for scripts
+chmod +x -R WireGuard-Tunnel
+
+cd WireGuard-Tunnel
+
+# ...
+
+# Create an alias for 'wgt' in the user's bashrc
+echo 'alias wgt="./root/WireGuard-Tunnel/wgt.sh"' >> ~/.bashrc
+
+# Source the bashrc to apply changes
+source ~/.bashrc
+
+echo "Setup complete. You can now use the 'wgt' command."
+
+# Optionally, show the menu after completing the setup
+./root/WireGuard-Tunnel/wgt.sh
