033fc0c1bcab1872d54c2d8b9932497fcb12f38a
smaghili <97967312+smaghili@users.noreply.github.com>
2024-02-09 01:19:58 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 # Set the GitHub repository URL
 repo_url="https://github.com/smaghili/WireGuard-Tunnel.git"
+
 # Clone the GitHub repository
-git clone "$repo_url"
+git clone "$repo_url" || { echo "Failed to clone the repository. Exiting."; exit 1; }
+
+# Navigate to the cloned repository directory
+cd WireGuard-Tunnel || { echo "Failed to navigate to the repository directory. Exiting."; exit 1; }
+
 # Set execute permissions for scripts
-chmod +x -R WireGuard-Tunnel
-cd WireGuard-Tunnel
-sed -i 's/\r$//' wgt.sh
+chmod -R +x . || { echo "Failed to set execute permissions. Exiting."; exit 1; }
+
+# Remove carriage return (CR) characters from wgt.sh
+sed -i 's/\r$//' wgt.sh || { echo "Failed to remove CR characters from wgt.sh. Exiting."; exit 1; }
+
 # Create an alias for 'wgt' in the user's bashrc
-echo 'alias wgt="./root/WireGuard-Tunnel/wgt.sh"' >> ~/.bashrc
+echo 'alias wgt="~/WireGuard-Tunnel/wgt.sh"' >> ~/.bashrc
+
 # Source the bashrc to apply changes
 source ~/.bashrc
+
 echo "Setup complete. You can now use the 'wgt' command."
-# Optionally, show the menu after completing the setup
-./root/WireGuard-Tunnel/wgt.sh
