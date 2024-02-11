794eed62d597696eeed502eec246c82766a24109
smaghili <97967312+smaghili@users.noreply.github.com>
2024-02-11 22:47:37 +0330

    Update set-route.sh

--- set-route.sh
+++ set-route.sh
@@ -5,3 +5,8 @@ if ! ip route show | grep -q "$server_ip"; then
 else
     echo "Route already exists. No action needed."
 fi
+if ! ip route show | grep -q "$1"; then
+    ip route add $1 via $2 dev $3
+else
+    echo "Route already exists. No action needed."
+fi
